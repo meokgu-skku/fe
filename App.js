41b51f6f36e0eede0f089e53d31fce985d5a2270
Eum SeungJu <109807198+sjsjmine129@users.noreply.github.com>
2024-04-04 23:37:18 +0900

    Add/FirstSetting (#2)

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView style={{}}>
+      <ScrollView style={{}}>
+        <View
+          style={{
+            backgroundColor: 'white',
+          }}>
+          <Text>TEST!!!</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
