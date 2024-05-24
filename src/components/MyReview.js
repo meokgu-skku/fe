95631697e594bf77f3fc051888a3d4a78bfdd760
jhpark223 <thesound08@naver.com>
2024-05-25 01:27:34 +0900

    mypagescreen only UI

---
+++ src/components/MyReview.js
@@ -0,0 +1,101 @@
+import React, {
+    useState,
+    useCallback,
+    useEffect,
+    useContext,
+    useRef,
+  } from 'react';
+  import {
+    View,
+    Text,
+    ScrollView,
+    StyleSheet,
+  } from 'react-native';
+  import {
+    COLOR_WHITE,
+    COLOR_PRIMARY,
+    COLOR_TEXT70GRAY,
+  } from '../assets/color';
+  import { useNavigation } from '@react-navigation/native';
+  import AppContext from './AppContext';
+  import { Dimensions } from 'react-native';
+  import StoreCompo from './StoreCompo';
+  
+  const windowWidth = Dimensions.get('window').width;
+  
+  export default function MyReview(props) {
+    const navigation = useNavigation();
+    const context = useContext(AppContext);
+  
+    const scrollViewRef = useRef();
+  
+    const { myReviews } = props;
+  
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    const handleScroll = event => {
+      const offsetX = event.nativeEvent.contentOffset.x;
+      const pageWidth = event.nativeEvent.layoutMeasurement.width;
+      const currentPage = Math.floor(offsetX / pageWidth + 0.5);
+      setCurrentIndex(currentPage);
+    };
+  
+    const Indicator = Array.from({ length: myReviews.length }, (_, index) => (
+      <View
+        key={index.toString()}
+        style={{
+          width: 4,
+          height: 4,
+          borderRadius: 2,
+          marginHorizontal: 3.5,
+          backgroundColor: index === currentIndex ? COLOR_PRIMARY : '#D9D9D9',
+        }}
+      />
+    ));
+  
+    return (
+      <View style={styles.myReview}>
+        <Text style={styles.myReviewTitle}>사용자&apos;s 리뷰</Text>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          style={{ marginTop: 10 }}
+          horizontal={true}
+          pagingEnabled
+          onScroll={handleScroll}
+          scrollEventThrottle={16}
+          ref={scrollViewRef}>
+          {myReviews.map((reviewData, index) => {
+            return <StoreCompo key={index.toString()} storeData={reviewData} index={index} />;
+          })}
+        </ScrollView>
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 8 }}>
+          {Indicator}
+        </View>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    myReview: {
+      marginTop: 15,
+      width: windowWidth - 32,
+      padding: 12,
+      paddingHorizontal: 10,
+      backgroundColor: COLOR_WHITE,
+      borderRadius: 10,
+      shadowOffset: {
+        width: 0,
+        height: 4,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 4, // for Android
+    },
+    myReviewTitle: {
+      fontSize: 20,
+      color: COLOR_TEXT70GRAY,
+      fontWeight: '700',
+    },
+  });
+  
--- src/screens/mypage/MypageScreen.js
+++ src/screens/mypage/MypageScreen.js
@@ -1,57 +1,126 @@
-import React, {useState, useCallback, useEffect} from 'react';
-import {View, Text, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
-import {
-  COLOR_WHITE,
-  COLOR_BACKGROUND,
-  COLOR_GRAY,
-  COLOR_PRIMARY,
-} from '../../assets/color';
-import AnimatedButton from '../../components/AnimationButton';
-import {useNavigation} from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Header from '../../components/Header';
+import { COLOR_BACKGROUND, COLOR_PRIMARY } from '../../assets/color';
+import MyReview from '../../components/MyReview';
+import MyStore from '../../components/MyStore';
 
 export default function MypageScreen() {
   const navigation = useNavigation();
 
+  const [myReviews, setMyReviews] = useState([
+    {
+      name: '율천회관',
+      category: '한식',
+      menu: '육회비빔밥',
+      image: 'https://d2da4yi19up8sp.cloudfront.net/product/max.jpeg',
+      score: 4.5,
+      reviewCount: 100,
+      heartCount: 20,
+      firstReview: {
+        reviewer: '엄승주',
+        body: '가게 내부 깨끗하고, 서비스 친절해서 개좋음. 기대안하고 첨 갔는데 걍 인생 oo 맛봄. 지림.',
+      },
+    },
+    {
+      name: '무대뽀 핫도그',
+      category: '술집',
+      menu: '핫도그',
+      image: 'https://d2da4yi19up8sp.cloudfront.net/product/pro.jpeg',
+      score: 4.5,
+      reviewCount: 100,
+      heartCount: 20,
+      firstReview: {
+        reviewer: '엄승주',
+        body: '가게 내부 깨끗하고, 서비스 친절해서 개좋음. 기대안하고 첨 갔는데 걍 인생 oo 맛봄. 지림.',
+      },
+    },
+    {
+      name: '무대뽀 핫도그',
+      category: '술집',
+      menu: '핫도그',
+      image: 'https://d2da4yi19up8sp.cloudfront.net/product/pro.jpeg',
+      score: 4.5,
+      reviewCount: 100,
+      heartCount: 20,
+      firstReview: {
+        reviewer: '엄승주',
+        body: '가게 내부 깨끗하고, 서비스 친절해서 개좋음. 기대안하고 첨 갔는데 걍 인생 oo 맛봄. 지림.',
+      },
+    },
+  ]);
+
+  const [myStoresData, setMyStoresData] = useState([
+    {
+      name: '율천회관',
+      image: 'https://d2da4yi19up8sp.cloudfront.net/product/pro.jpeg',
+    },
+    {
+      name: '무대뽀 핫도그',
+      image: 'https://d2da4yi19up8sp.cloudfront.net/product/max.jpeg',
+    },
+    {
+      name: '무대뽀 핫도그',
+      image: 'https://d2da4yi19up8sp.cloudfront.net/product/max.jpeg',
+    },
+  ]);
+
   return (
     <>
       <Header title={'마이 프로필'} isBackButton={false} />
-      <View style={styles.entire}>
-        <Text style={styles.textMain}>MypageScreen</Text>
-        <AnimatedButton
+      <ScrollView contentContainerStyle={styles.entire}>
+        <Image
+          style={[styles.myPageItem, styles.myPageItemLayout]}
+          resizeMode="cover"
+          source={require("../../assets/skku.png")}
+        />
+        <TouchableOpacity
+          style={styles.text6Position}
           onPress={() => {
             navigation.navigate('UserDataChange');
           }}
-          style={styles.buttonTest}>
-          <Text style={styles.buttonText}>프로필 정보 수정 페이지 이동</Text>
-        </AnimatedButton>
-      </View>
+        >
+          <Text style={styles.text6}>사용자 이름</Text>
+          <Image
+            style={styles.arrowIcon}
+            resizeMode="contain"
+            source={require("../../assets/right-arrow.png")}
+          />
+        </TouchableOpacity>
+        <MyStore passData={myStoresData} />
+        <MyReview myReviews={myReviews} />
+        <View style={{ height: 100 }} />
+      </ScrollView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
   entire: {
-    flex: 1,
     backgroundColor: COLOR_BACKGROUND,
-    justifyContent: 'center',
     alignItems: 'center',
   },
-  textMain: {
-    fontSize: 50,
-    color: COLOR_PRIMARY,
+  myPageItem: {
+    width: 100,
+    height: 100,
   },
-  buttonTest: {
-    backgroundColor: COLOR_PRIMARY,
-    padding: 10,
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
+  myPageItemLayout: {
+    marginTop: 20,
+    marginBottom: 20,
   },
-  buttonText: {
+  text6: {
     fontSize: 20,
-    color: COLOR_WHITE,
-    fontWeight: 'bold',
-    alignSelf: 'center',
+    color: COLOR_PRIMARY,
+    marginLeft: 20,
+    marginRight: 5
+  },
+  text6Position: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  arrowIcon: {
+    width: 20,
+    height: 20,
   },
 });
