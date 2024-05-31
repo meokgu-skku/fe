f5c088d08fae7b319f4624d7f948e34f5d8e39a8
sinkyoungdeok <tkagmd1@naver.com>
2024-06-01 03:32:38 +0900

    [KAN-120] 마이페이지 > 내 리뷰: 다른 음식점 리뷰로 보이는 버그 수정

--- src/components/StoreCompoForR.js
+++ src/components/StoreCompoForR.js
@@ -25,10 +25,10 @@ export default function StoreCompoForR(props) {
   const [storeInfo, setStoreInfo] = useState([]);
 
   const restaurantDetail = async () => {
-    console.log('Id: ', storeData.id);
+    console.log('Id: ', storeData.restaurantId);
     try {
       const response = await axios.get(
-        `${API_URL}/v1/restaurants/${storeData.id}`,
+        `${API_URL}/v1/restaurants/${storeData.restaurantId}`,
         {
           headers: {Authorization: `Bearer ${context.accessToken}`},
         },
@@ -86,7 +86,7 @@ export default function StoreCompoForR(props) {
         </View>
         <View style={{flex: 1, marginLeft: 12}}>
           <Text style={styles.storeName} numberOfLines={1}>
-            {storeInfo.name}
+            {storeInfo.name}.1
           </Text>
           <View
             style={{
