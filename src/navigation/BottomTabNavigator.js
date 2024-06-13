e6c4c055a2367249b1eb92c8e9f3d22d3602d9e2
sjsjmine129 <sjsjmine129@gmail.com>
2024-06-13 09:36:21 +0900

    Fix/safeArea

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -33,7 +33,7 @@ import MypageScreen from '../screens/mypage/MypageScreen';
 import UserDataChangeScreen from '../screens/mypage/UserDataChangeScreen';
 
 const BottomTab = createBottomTabNavigator();
-const iconSize = 35;
+const iconSize = 17;
 
 const customCardStyleInterpolator = ({current, next, layouts}) => {
   return {
@@ -67,100 +67,112 @@ const customCardStyleInterpolator = ({current, next, layouts}) => {
 const HomeStack = createStackNavigator();
 function HomeNavigator() {
   return (
-    <HomeStack.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        headerShown: false,
-        // gestureEnabled: true,
-        // cardOverlayEnabled: true,
-        // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-        cardStyleInterpolator: customCardStyleInterpolator,
-      }}>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-    </HomeStack.Navigator>
+    <Safe color={COLOR_PRIMARY}>
+      <HomeStack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+          // gestureEnabled: true,
+          // cardOverlayEnabled: true,
+          // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          cardStyleInterpolator: customCardStyleInterpolator,
+        }}>
+        <HomeStack.Screen name="Home" component={HomeScreen} />
+      </HomeStack.Navigator>
+    </Safe>
   );
 }
 
 const MapStack = createStackNavigator();
 function MapNavigator() {
   return (
-    <MapStack.Navigator
-      initialRouteName="Map"
-      screenOptions={{
-        headerShown: false,
-        // gestureEnabled: true,
-        // cardOverlayEnabled: true,
-        // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-        cardStyleInterpolator: customCardStyleInterpolator,
-      }}>
-      <MapStack.Screen name="Map" component={MapScreen} />
-    </MapStack.Navigator>
+    <Safe color={COLOR_PRIMARY}>
+      <MapStack.Navigator
+        initialRouteName="Map"
+        screenOptions={{
+          headerShown: false,
+          // gestureEnabled: true,
+          // cardOverlayEnabled: true,
+          // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          cardStyleInterpolator: customCardStyleInterpolator,
+        }}>
+        <MapStack.Screen name="Map" component={MapScreen} />
+      </MapStack.Navigator>
+    </Safe>
   );
 }
 
 const ListStack = createStackNavigator();
 function ListNavigator() {
   return (
-    <ListStack.Navigator
-      initialRouteName="ListMain"
-      screenOptions={{
-        headerShown: false,
-        // gestureEnabled: true,
-        // cardOverlayEnabled: true,
-        // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-        cardStyleInterpolator: customCardStyleInterpolator,
-      }}>
-      <ListStack.Screen name="ListMain" component={ListMainScreen} />
-    </ListStack.Navigator>
+    <Safe color={COLOR_PRIMARY}>
+      <ListStack.Navigator
+        initialRouteName="ListMain"
+        screenOptions={{
+          headerShown: false,
+          // gestureEnabled: true,
+          // cardOverlayEnabled: true,
+          // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          cardStyleInterpolator: customCardStyleInterpolator,
+        }}>
+        <ListStack.Screen name="ListMain" component={ListMainScreen} />
+      </ListStack.Navigator>
+    </Safe>
   );
 }
 
 const MypageStack = createStackNavigator();
 function MypageNavigator() {
   return (
-    <MypageStack.Navigator
-      initialRouteName="Mypage"
-      screenOptions={{
-        headerShown: false,
-        // gestureEnabled: true,
-        // cardOverlayEnabled: true,
-        // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-        cardStyleInterpolator: customCardStyleInterpolator,
-      }}>
-      <MypageStack.Screen name="Mypage" component={MypageScreen} />
-      <MypageStack.Screen
-        name="UserDataChange"
-        component={UserDataChangeScreen}
-      />
-    </MypageStack.Navigator>
+    <Safe color={COLOR_PRIMARY}>
+      <MypageStack.Navigator
+        initialRouteName="Mypage"
+        screenOptions={{
+          headerShown: false,
+          // gestureEnabled: true,
+          // cardOverlayEnabled: true,
+          // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          cardStyleInterpolator: customCardStyleInterpolator,
+        }}>
+        <MypageStack.Screen name="Mypage" component={MypageScreen} />
+        <MypageStack.Screen
+          name="UserDataChange"
+          component={UserDataChangeScreen}
+        />
+      </MypageStack.Navigator>
+    </Safe>
   );
 }
 
 export default function BottomTabNavigator() {
   changeNavigationBarColor(COLOR_PRIMARY);
   return (
-    <Safe color={COLOR_PRIMARY}>
+    <>
       <StatusBar backgroundColor={COLOR_PRIMARY} />
       <BottomTab.Navigator
         initialRouteName="HomeNavigator"
         screenOptions={{
-          headerShown: false,
           tabBarStyle: {
-            height: 56,
+            height: Platform.OS == 'android' ? 48 : 76,
             backgroundColor: COLOR_PRIMARY,
-            // backgroundColor: 'red',
+            alignContent: 'center',
+            paddingTop: 10,
           },
-          tabBarShowLabel: false,
+          headerShown: false,
           tabBarLabelStyle: {
-            fontFamily: 'NotoSansKR-Regular',
+            fontSize: 12,
+            marginTop: 5,
+            fontFamily: 'NanumSquareRoundB',
           },
-          tabBarActiveTintColor: COLOR_PRIMARY,
+          tabBarLabelPosition: 'below-icon',
+          tabBarInactiveTintColor: '#929593',
+          tabBarActiveTintColor: COLOR_WHITE,
         }}>
         <BottomTab.Screen
           name="HomeNavigator"
           component={HomeNavigator}
           options={{
-            tabBarShowLabel: false,
+            tabBarShowLabel: true,
             tabBarLabel: '홈',
             tabBarIcon: props => (
               <SvgXml
@@ -178,7 +190,7 @@ export default function BottomTabNavigator() {
           name="MapNavigator"
           component={MapNavigator}
           options={{
-            tabBarShowLabel: false,
+            tabBarShowLabel: true,
             tabBarLabel: '지도',
             tabBarIcon: props => (
               <SvgXml
@@ -196,7 +208,7 @@ export default function BottomTabNavigator() {
           name="ListNavigator"
           component={ListNavigator}
           options={{
-            tabBarShowLabel: false,
+            tabBarShowLabel: true,
             tabBarLabel: '리스트',
             tabBarIcon: props => (
               <SvgXml
@@ -214,7 +226,7 @@ export default function BottomTabNavigator() {
           name="MypageNavigator"
           component={MypageNavigator}
           options={{
-            tabBarShowLabel: false,
+            tabBarShowLabel: true,
             tabBarLabel: '프로필',
             tabBarIcon: props => (
               <SvgXml
@@ -231,6 +243,6 @@ export default function BottomTabNavigator() {
           }}
         />
       </BottomTab.Navigator>
-    </Safe>
+    </>
   );
 }
